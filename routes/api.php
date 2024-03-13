fc06f3ad35ea5290baa6189a3cf9f90823ea114e
MohannadBT <xsmile3x@gmail.com>
2024-03-13 16:54:31 +0300

    .6

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use \App\Http\Controllers\Api\V1\Auth;
-use \App\Http\Controllers\Api\V1\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Api\V1\Auth;
+use App\Http\Controllers\Api\V1\ZoneController;
+use \App\Http\Controllers\Api\V1\VehicleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +23,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+Route::post('auth/logout', Auth\LogoutController::class);
+
+Route::get('zones', [ZoneController::class,'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
-    Route::apiResource('vehicles', VehicleController::class);});
-
-Route::post('auth/logout', Auth\LogoutController::class);
+    Route::apiResource('vehicles', VehicleController::class);
+    // Route::apiResource('zones', ZoneController::class);
+})
+;
