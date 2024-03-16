809886db688e71518a4451b00e75628066110fb0
MohannadBT <xsmile3x@gmail.com>
2024-03-16 17:54:23 +0300

    trying fixing zonetest

--- tests/Feature/ZoneTest.php
+++ tests/Feature/ZoneTest.php
@@ -3,12 +3,12 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ZoneTest extends TestCase
 {
     use RefreshDatabase;
+
     public function testPublicUserCanGetAllZones()
     {
         $response = $this->getJson('/api/v1/zones');
