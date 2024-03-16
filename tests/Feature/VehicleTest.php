ff936b9c53b558be3e78886835cf3c2f2bada8c1
MohannadBT <xsmile3x@gmail.com>
2024-03-16 17:52:42 +0300

    trying fixes

--- tests/Feature/VehicleTest.php
+++ tests/Feature/VehicleTest.php
@@ -11,80 +11,78 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class VehicleTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_user_can_get_their_own_vehicle(): void
+    public function testUserCanGetTheirOwnVehicles()
     {
-        $ali = User::factory()->create();
-        $vehicleForAli = Vehicle::factory()->create([
-            "user_id"=> $ali->id,
+        $john = User::factory()->create();
+        $vehicleForJohn = Vehicle::factory()->create([
+            'user_id' => $john->id
         ]);
 
-        $hadi = User::factory()->create();
-        $vehicleForHadi = Vehicle::factory()->create([
-            "user_id"=> $hadi->id,
+        $adam = User::factory()->create();
+        $vehicleForAdam = Vehicle::factory()->create([
+            'user_id' => $adam->id
         ]);
 
-        $response = $this->actingAs($ali)->getJson('/api/v1/vehicles');
+        $response = $this->actingAs($john)->getJson('/api/v1/vehicles');
 
         $response->assertStatus(200)
-        ->assertJsonStructure(['data'])
-        ->assertJsonCount(1, 'data')
-        ->assertJsonPath('data.0.plate_number', $vehicleForAli->plate_number)
-        ->assertJsonMissing($vehicleForHadi->toArray());
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(1, 'data')
+            ->assertJsonPath('data.0.plate_number', $vehicleForJohn->plate_number)
+            ->assertJsonMissing($vehicleForAdam->toArray());
     }
 
-    public function test_user_can_create_vehicle()
+    public function testUserCanCreateVehicle()
     {
         $user = User::factory()->create();
 
         $response = $this->actingAs($user)->postJson('/api/v1/vehicles', [
-            'plate_number' => 'TST001'
+            'plate_number' => 'AAA111',
         ]);
 
         $response->assertStatus(201)
-        ->assertJsonStructure(['data'])
-        ->assertJsonCount(2, 'date')
-        ->assertJsonStructure([
-            'data' => ['0' => 'plate_number'],
-        ])
-        ->assertJsonPath('data.0.plate_number', 'TST001');
-
-        $this->assertDataBaseHas('vehicles', [
-            'plate_number' => 'TST001'
-        ]);
-    }
-
-    public function test_user_can_update_their_vehicle()
-    {
-        $user = User::factory()->create();
-        $vehicle = Vehicle::factory()->create([
-            'user_id' => $user->id,
-        ]);
-
-        $response = $this->actingAs($user)->putJson('/api/v1/vehicles' . $vehicle->id, [
-            'plate_number' => 'UPD999',
-        ]);
-
-        $response->assertStatus(202)
-        ->assertJsonStructure(['plate_number'])
-        ->assertJsonPath('plate_number','UPD999');
+            ->assertJsonStructure(['data'])
+            ->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => ['0' => 'plate_number'],
+            ])
+            ->assertJsonPath('data.plate_number', 'AAA111');
 
         $this->assertDatabaseHas('vehicles', [
-            'plate_number' => 'UPD999',
+            'plate_number' => 'AAA111',
         ]);
     }
 
-    public function test_user_can_delete_their_vehicle()
+    public function testUserCanUpdateTheirVehicle()
     {
         $user = User::factory()->create();
         $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
 
-        $response = $this->actingAs($user)->deleteJson('/api/v1/vehicles' . $vehicle->id);
+        $response = $this->actingAs($user)->putJson('/api/v1/vehicles/' . $vehicle->id, [
+            'plate_number' => 'AAA123',
+        ]);
+
+        $response->assertStatus(202)
+            ->assertJsonStructure(['plate_number'])
+            ->assertJsonPath('plate_number', 'AAA123');
+
+        $this->assertDatabaseHas('vehicles', [
+            'plate_number' => 'AAA123',
+        ]);
+    }
+
+    public function testUserCanDeleteTheirVehicle()
+    {
+        $user = User::factory()->create();
+        $vehicle = Vehicle::factory()->create(['user_id' => $user->id]);
+
+        $response = $this->actingAs($user)->deleteJson('/api/v1/vehicles/' . $vehicle->id);
 
         $response->assertNoContent();
 
         $this->assertDatabaseMissing('vehicles', [
             'id' => $vehicle->id,
-            'deleted_at' => null
-        ])->assertDatabaseCount('vehicles', 1);
+            'deleted_at' => NULL
+        ])->assertDatabaseCount('vehicles', 1); // we have SoftDeletes, remember?
     }
 }
