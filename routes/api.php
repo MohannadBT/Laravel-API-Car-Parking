286feedde4ca90afad5c25cf56062f912ac8ed71
MohannadBT <xsmile3x@gmail.com>
2024-04-18 03:52:49 +0300

    fix issue with logout

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
-Route::post('auth/logout', Auth\LogoutController::class);
 
 Route::get('zones', [ZoneController::class,'index']);
 
@@ -36,7 +35,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('parkings/start', [ParkingController::class, 'start']);
     Route::get('parkings/{parking}', [ParkingController::class, 'show']);
     Route::put('parkings/{parking}', [ParkingController::class, 'stop']);
-
+    Route::post('auth/logout', Auth\LogoutController::class);
     // Route::apiResource('zones', ZoneController::class);
 })
 ;
